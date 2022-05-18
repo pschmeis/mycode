41a968fe3a410f7723a5c5aba9e13524e69c54eb
Phil <pschmeis@users.noreply.github.com>
2022-05-18 16:29:33 +0000

    day 3

---
+++ dictest.py
@@ -0,0 +1,14 @@
+Cable = {
+	"Real Name": "Nathan Summers",
+	"Creators": ["Chris Claremont", "Rick Leonardi", "Louise Simonson", "Rob Liefeld"],
+	"Team Affliations": ["X-Men", "X-Force", "Askani", "Six Pack", "The Twelve", "New Mutants", "The Underground"],
+	"Aliases": ["Nathan Winters", "Nathan Dayspring", "Askani'son", "Soldier X", "Chosen One", "Traveler"],
+	"Base of Operations": "Often in Flux", 
+	"Powers": ["Telepathy", "Telekinesis", "Technopathy", "Enhanced Physical Attributes"]
+}
+
+DrStrange={'Real Name':'Stephen Strange','Abilities':['Martial Arts','Energy Blasts','Sword Fighting'],'Aliases':['Sherlock','Sorcerer Supreme','Vincent Stephens'],'Universe':'Marvel','Education':['MD','Extensive Sorcery Training']}
+
+
+print(f"Dr Strange, aka {DrStrange['Real Name']}, aka {DrStrange['Aliases'][1]}, was a member of the {DrStrange['Universe']} universe. He is known for his {DrStrange['Abilities'][1]} and {DrStrange['Education'][1]}.")
+
---
+++ input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main(): 
+    name = input("What is your name? ")
+    day = input("what is the day? ")
+
+    print("\nHello, " + name + "! Happy " + day + "!")
+
+main()
